86e1b244957060b1048995e93efb1b548862e9ef
Krishna Kanth <krishnamk2496@gmail.com>
2019-08-04 09:10:39 +0530

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-//this is index.js
-//i have created this without knowing the purpose of this file
-// i dont know how index.js works in connecting other modules(files)
-console.log("hello world this is printing from index.js file");
